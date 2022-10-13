028337107111faa4d8dc69514ac6f88cb7cdeb36
Augusto César Bottega Agostini <Augusto.Agostini@edu.pucrs.br>
2022-10-13 09:04:27 -0300

    running movement complete

--- Player.gd
+++ Player.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var walkSpeed = 150
+export (int) var runSpeed = 300
 
+var speed
 var velocity = Vector2.ZERO
+var movement = 'Walk'
 
 onready var target := position
 onready var sprite := $AnimatedSprite
@@ -11,16 +14,23 @@ func get_input():
 	velocity.x = Input.get_action_strength("right")-Input.get_action_strength("left")
 	velocity.y = Input.get_action_strength("down")-Input.get_action_strength("up")
 	
+	if Input.is_action_pressed("run"):
+		movement = 'Run'
+		speed = runSpeed
+	else: 
+		movement = 'Walk'
+		speed = walkSpeed
+	
 	if velocity.x > 0:
 		sprite.flip_h = false
-		sprite.play('Walk')
+		sprite.play(movement)
 	elif velocity.x < 0:
 		sprite.flip_h = true
-		sprite.play('Walk')
+		sprite.play(movement)
 	elif velocity.y > 0:
-		sprite.play('Walk')
+		sprite.play(movement)
 	elif velocity.y < 0:
-		sprite.play('Walk')
+		sprite.play(movement)
 	else:
 		sprite.play('Idle')
 	velocity = velocity.normalized() * speed
