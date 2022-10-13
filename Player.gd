60c3c98802df0799de250feb862f93cc826d5468
Augusto <guto9bottega@gmail.com>
2022-10-12 21:45:19 -0300

    animação feita com problema de jitter e mapa básico para referencia

--- Player.gd
+++ Player.gd
@@ -2,18 +2,27 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 
-var velocity = Vector2()
+var velocity = Vector2.ZERO
+
+onready var target := position
+onready var sprite := $AnimatedSprite
 
 func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
+	velocity.x = Input.get_action_strength("right")-Input.get_action_strength("left")
+	velocity.y = Input.get_action_strength("down")-Input.get_action_strength("up")
+	
+	if velocity.x > 0:
+		sprite.flip_h = false
+		sprite.play('Walk')
+	elif velocity.x < 0:
+		sprite.flip_h = true
+		sprite.play('Walk')
+	elif velocity.y > 0:
+		sprite.play('Walk')
+	elif velocity.y < 0:
+		sprite.play('Walk')
+	else:
+		sprite.play('Idle')
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
