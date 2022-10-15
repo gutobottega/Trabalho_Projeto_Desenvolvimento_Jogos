1172fe43d13778421abd4abea7c4b5efd4bb3dea
psj <jardimpedro21@gmail.com>
2022-10-15 16:04:41 -0300

    Map_child_v0.2

--- Scene_child2.gd
+++ Scene_child2.gd
@@ -1,4 +1,4 @@
-extends CollisionShape2D
+extends Node2D
 
 
 # Declare member variables here. Examples:
@@ -8,7 +8,8 @@ extends CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#$Floor.centered_textures = true
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
